f898eab364682f8b3c81a1a0048e19a558eab9bb
Carsten Pleiser <carsten@eventtechpartner.com>
2019-07-30 21:40:44 +0100

    Added Passport & Express Session

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -11,6 +13,15 @@ app.set('view-engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/')
   .get((req, res) => {
