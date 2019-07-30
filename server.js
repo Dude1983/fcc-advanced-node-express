da41407d46cecc0a6803be379fca7083c4308eca
Carsten Pleiser <carsten@eventtechpartner.com>
2019-07-30 21:26:39 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
+
+app.set('view-engine', 'pug');
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.route('/')
+  .get((req, res) => {
+    res.render(process.cwd() + '/views/pug/index.pug');
+  });
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log("Listening on port " + port);
+});
